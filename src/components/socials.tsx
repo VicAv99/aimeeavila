45cc495b656f3b34d0434819c64b009e2e0d0ecd
Victor Avila <vicav99@gmail.com>
2023-05-20 00:26:02 -0700

    refactor: make socials client

--- src/components/socials.tsx
+++ src/components/socials.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import Link from "next/link";
 import React from "react";
 
@@ -8,19 +10,19 @@ export function Socials() {
   return (
     <div className="fixed bottom-0 w-full">
       <div className="bottom-0 float-right mb-5 mr-8">
-        <Button variant="outline" className="mr-1.5 bg-white">
+        <Button asChild variant="outline" className="mr-1.5 bg-white">
           <Link target="_blank" href="https://www.instagram.com/aimee_avila/">
             <Icon name="Instagram" />
           </Link>
         </Button>
-        <Button variant="outline" className="mx-1.5 bg-white">
+        <Button asChild variant="outline" className="mx-1.5 bg-white">
           <Link target="_blank" href="https://twitter.com/aimee_avila23">
             <Icon name="Twitter" />
           </Link>
         </Button>
-        <Button variant="outline" className="ml-1.5 bg-white">
-          <Link target="_blank" href="https://www.facebook.com/aimee.avila.73">
-            <Icon name="Facebook" />
+        <Button asChild variant="outline" className="ml-1.5 bg-white">
+          <Link target="_blank" href="https://www.linkedin.com/in/aimee-avila/">
+            <Icon name="Linkedin" />
           </Link>
         </Button>
       </div>
