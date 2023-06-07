4d54e922d7138371aa493c9b6184ba815f9d7418
Victor Avila <vicav99@gmail.com>
2023-06-06 22:59:49 -0700

    feat: update socials

--- src/components/socials.tsx
+++ src/components/socials.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 
 import Link from "next/link";
 import React from "react";
@@ -10,11 +10,6 @@ export function Socials() {
   return (
     <div className="fixed bottom-0 w-full">
       <div className="bottom-0 float-right mb-5 mr-8">
-        <Button asChild variant="outline" className="mr-1.5 bg-white">
-          <Link target="_blank" href="https://www.instagram.com/aimee_avila/">
-            <Icon name="Instagram" />
-          </Link>
-        </Button>
         <Button asChild variant="outline" className="mx-1.5 bg-white">
           <Link target="_blank" href="https://twitter.com/aimee_avila23">
             <Icon name="Twitter" />
@@ -23,6 +18,11 @@ export function Socials() {
         <Button asChild variant="outline" className="ml-1.5 bg-white">
           <Link target="_blank" href="https://www.linkedin.com/in/aimee-avila/">
             <Icon name="Linkedin" />
+          </Link>
+        </Button>
+        <Button asChild variant="outline" className="ml-1.5 bg-white">
+          <Link target="_blank" href="https://github.com/aimeeavila">
+            <Icon name="Github" />
           </Link>
         </Button>
       </div>
