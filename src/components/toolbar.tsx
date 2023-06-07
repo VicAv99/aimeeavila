19db05e78ca4e66c24efa8ebcddfaa7fa7409014
Victor Avila <vicav99@gmail.com>
2023-06-06 23:38:01 -0700

    feat: add links

--- src/components/toolbar.tsx
+++ src/components/toolbar.tsx
@@ -15,25 +15,30 @@ const ROUTES = [
     href: "#",
     title: "projects",
   },
+  {
+    href: "#",
+    title: "blog",
+  },
+  {
+    href: "#",
+    title: "contact",
+  },
 ];
 
 export function Toolbar() {
   return (
-    <div className="fixed w-full px-10 py-3 shadow-md md:shadow-lg md:py-5 bg-background">
+    <div className="fixed w-full py-1 shadow-md px-7 md:shadow-lg bg-primary">
       <div className="container flex justify-between">
-        <div className="flex items-center gap-3">
-          <div className="relative w-14 h-14">
+        <div className="flex items-center">
+          <div className="relative w-12 h-16">
             <Image
               fill
-              src="https://media.licdn.com/dms/image/D5635AQEP8bcyA5hOGA/profile-framedphoto-shrink_400_400/0/1677284821411?e=1684926000&v=beta&t=-qD9kxTXyHwGfvwgY1-Yl0P6BD5IUqsKOPdhtS8A9G8"
+              src="/images/logo-4.png"
               alt=""
-              style={{ objectFit: "cover" }}
-              className="rounded-full"
+              style={{ objectFit: "fill" }}
             />
           </div>
-          <h3 className="hidden text-lg font-bold uppercase whitespace-nowrap md:block">
-            Aimee Avila
-          </h3>
+          <h3 className="hidden text-lg whitespace-nowrap md:block">Aimee</h3>
         </div>
         <div className="flex items-center justify-end w-full gap-0.5">
           {ROUTES.map((route) => (
@@ -44,7 +49,7 @@ export function Toolbar() {
               className="px-1 md:px-2"
             >
               <Link href={route.href}>
-                <span className="font-bold uppercase text-slate-700 hover:text-teal-600">
+                <span className="text-base font-light text-black lowercase hover:underline">
                   {route.title}
                 </span>
               </Link>
