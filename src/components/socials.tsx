469f9cb351f8e2f8566e4f1db969482c417da3a6
Victor Avila <vicav99@gmail.com>
2023-06-19 21:38:52 -0700

    feat: update socials for mobile

--- src/components/socials.tsx
+++ src/components/socials.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import Link from "next/link";
 import React from "react";
 
@@ -8,24 +6,22 @@ import { Icon } from "./ui/icon";
 
 export function Socials() {
   return (
-    <div className="fixed bottom-0 w-full">
-      <div className="bottom-0 float-right mb-5 mr-8">
-        <Button asChild variant="outline" className="mx-1.5 bg-white">
-          <Link target="_blank" href="https://twitter.com/aimee_avila23">
-            <Icon name="Twitter" />
-          </Link>
-        </Button>
-        <Button asChild variant="outline" className="ml-1.5 bg-white">
-          <Link target="_blank" href="https://www.linkedin.com/in/aimee-avila/">
-            <Icon name="Linkedin" />
-          </Link>
-        </Button>
-        <Button asChild variant="outline" className="ml-1.5 bg-white">
-          <Link target="_blank" href="https://github.com/aimeeavila">
-            <Icon name="Github" />
-          </Link>
-        </Button>
-      </div>
+    <div className="flex justify-center mb-5 space-x-3">
+      <Button asChild variant="outline" className="bg-stone-300">
+        <Link target="_blank" href="https://www.linkedin.com/in/aimee-avila/">
+          <Icon name="Linkedin" />
+        </Link>
+      </Button>
+      <Button asChild variant="outline" className="bg-stone-300">
+        <Link target="_blank" href="https://twitter.com/aimee_avila23">
+          <Icon name="Twitter" />
+        </Link>
+      </Button>
+      <Button asChild variant="outline" className="bg-stone-300">
+        <Link target="_blank" href="https://github.com/aimeeavila">
+          <Icon name="Github" />
+        </Link>
+      </Button>
     </div>
   );
 }
