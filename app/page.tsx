41370aac1a4f7ae403929e238dbd5a2ca8384630
Victor Avila <vicav99@gmail.com>
2023-08-24 16:56:47 -0700

    style: grid about me and skills

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,10 @@ export default function Home() {
   return (
     <main className="mx-auto max-w-4xl px-6 lg:px-16 space-y-40">
       <MainSection />
-      <AboutMeSection />
-      <SkillsSection />
+      <div className="grid grid-cols-1 md:grid-cols-2">
+        <AboutMeSection />
+        <SkillsSection />
+      </div>
     </main>
   );
 }
