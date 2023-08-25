1d59a0d9daaedcb81762982b27a055716473e3e4
Victor Avila <vicav99@gmail.com>
2023-08-24 19:43:10 -0700

    style: fix layout issue

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <main className="mx-auto max-w-4xl px-6 lg:px-16 space-y-40">
       <MainSection />
-      <div className="grid grid-cols-1 md:grid-cols-2">
+      <div className="grid grid-cols-1 md:grid-cols-2 space-y-40 md:space-y-0">
         <AboutMeSection />
         <SkillsSection />
       </div>
