d6e678b76b22d5d1b539e840594a4f39955ad94c
Victor Avila <vicav99@gmail.com>
2023-08-24 21:28:48 -0700

    feat: revalidate

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -7,6 +7,8 @@ import { Profile } from "~/lib/models/profile";
 import { client } from "~/sanity/lib/client";
 import { profileQuery } from "~/sanity/lib/queries";
 
+export const revalidate = 900;
+
 export default async function Home() {
   const profile = await client.fetch<Profile>(profileQuery);
 
